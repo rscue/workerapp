ab16735c2e3074a0973ee25654f6394ed06d54db
Ignacio Glinsek <ignacio.glinsek@southworks.com>
2017-06-20 15:26:42 -0300

    Add HomeContainer mockup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react'
-import { StatusBar } from 'react-native'
-import { Container, StyleProvider, View, Text } from 'native-base'
+import { StatusBar, View } from 'react-native'
+import { Container, StyleProvider } from 'native-base'
 
 import getTheme from '../theme/components'
 import rscueColors from '../theme/variables/rscueColors'
+
+import HomeContainer from './HomeContainer';
 
 class App extends Component {
     render() {
         return (
             <StyleProvider style={getTheme(rscueColors)} >
-                <Container>
+                <Container >
                     <StatusBar barStyle='light-content' />
-                    <View >
-                        <Text >
-                            Welcome to React Native!
-        </Text>
-                        <Text >
-                            To get started, edit index.android.js
-        </Text>
-                        <Text >
-                            Double tap R on your keyboard to reload,{'\n'}
-                            Shake or press menu button for dev menu
-        </Text>
-                    </View>
+                    <HomeContainer />
                 </Container>
             </StyleProvider>
         )
---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import {
+    Container, Header, Body, Content, Title, Text, Left, Right, Icon,
+    Switch, ListItem, Grid, Row, Col, Card, CardItem, H2, List, Button, Separator
+} from 'native-base';
+
+class HomeContainer extends Component {
+    render() {
+        return (
+            <Container>
+                <Header>
+                    <Body>
+                        <Title>Inicio</Title>
+                    </Body>
+                </Header>
+                <Content>
+                    <ListItem last>
+                        <Body style={{ alignItems: 'center', marginTop: 20 }}>
+                            <H2>Juan Perez</H2>
+                        </Body>
+                    </ListItem>
+                    <ListItem last style={{ height: 355 }}>
+                        <Body style={{ alignItems: 'center' }}>
+                            <Image style={{ resizeMode: 'cover', width: 250, height: 250, borderRadius: 125 }} source={require('../assets/img/nobody.jpg')} />
+                        </Body>
+                    </ListItem>
+                    <Separator bordered />
+                    <ListItem button icon last>
+                        <Left>
+                            <Button style={{ backgroundColor: '#FF9501' }} >
+                                <Icon active name="boat" />
+                            </Button>
+                        </Left>
+                        <Body>
+                            <Text>Cruise 350</Text>
+                        </Body>
+                        <Right>
+                            <Icon name="arrow-forward" />
+                        </Right>
+                    </ListItem>
+                    <ListItem icon last>
+                        <Left>
+                            <Button style={{ backgroundColor: '#FD3C2D' }} >
+                                <Icon name="help-buoy" />
+                            </Button>
+                        </Left>
+                        <Body>
+                            <Text>En servicio</Text>
+                        </Body>
+                        <Right>
+                            <Switch />
+                        </Right>
+                    </ListItem>
+                </Content >
+            </Container >
+        )
+    }
+}
+
+export default HomeContainer;
