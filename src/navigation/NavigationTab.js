f860399bf5a56bb0eff9f3f0107cef88141a31eb
Ignacio Glinsek <ignacio.glinsek@southworks.com>
2017-06-26 08:55:33 -0300

    Removed NavigationTab transition due to bug

--- src/navigation/NavigationTab.js
+++ src/navigation/NavigationTab.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
-import { addNavigationHelpers, Transitioner } from 'react-navigation';
-import { Easing, Animated, View, StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-    sceneStyle: {
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        left: 0,
-        bottom: 0
-    }
-});
+import { addNavigationHelpers } from 'react-navigation';
 
 const CustomTabBar = ({ navigation, router }) => {
     const { routes, index } = navigation.state;
@@ -33,52 +22,20 @@ const CustomTabBar = ({ navigation, router }) => {
     );
 };
 
-
-class NavigationTab extends Component {
-    _configureTransition = (transitionProps, prevTransitionProps) => {
-        return {
-            duration: 300,
-            easing: Easing.inOut(Easing.ease),
-        }
-    };
-
-    _renderScene = (transitionProps, scene) => {
-        const { position } = transitionProps;
-        const { index } = scene;
-        const opacity = position.interpolate({
-            inputRange: [index - 1, index, index + 1],
-            outputRange: [0, 1, 0],
-        });
-        // The prop `router` is populated when we call `createNavigator`.
-        const Scene = this.props.router.getComponentForRouteName(scene.route.routeName);
-        return (
-            <Animated.View style={[styles.sceneStyle, { opacity }]} key={scene.key} >
-                <Scene navigation={this.props.navigation} />
-            </Animated.View>
-        )
-    };
-
-    _render = (transitionProps, prevTransitionProps) => {
-        const scenes = transitionProps.scenes.map(scene => this._renderScene(transitionProps, scene));
-        return (
-            <View style={{ flex: 1 }}>
-                {scenes}
-            </View>
-        );
-    };
-
-    render() {
-        return (
-            <Container>
-                <Transitioner
-                    configureTransition={this._configureTransition}
-                    navigation={this.props.navigation}
-                    render={this._render}
-                />
-                <CustomTabBar navigation={this.props.navigation} router={this.props.router} />
-            </Container>
-        );
-    }
-}
+const NavigationTab = ({ router, navigation }) => {
+    const { routes, index } = navigation.state;
+    const ActiveScreen = router.getComponentForState(navigation.state);
+    return (
+        <Container>
+            <ActiveScreen
+                navigation={addNavigationHelpers({
+                    ...navigation,
+                    state: routes[index],
+                })}
+            />
+            <CustomTabBar navigation={navigation} router={router} />
+        </Container>
+    );
+};
 
 export default NavigationTab;
